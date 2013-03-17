f22b63038da989538677255dd8472dae5b8d2e6c
yluhadiya7 <yluhadiya7@gmail.com>
2013-03-17 05:35:16 +0530

    FirstCommentClassCommit

---
+++ php/includes/comment.php
@@ -0,0 +1,14 @@
+<?
+
+require_once './initialize_database.php';
+
+class Comment{
+
+	private $userName, $voteValue, $timeStamp, $commentString, $upVote, $downVote, $reportAbuseCount, $db;
+
+	public function __construct($keyword){
+
+	}
+}
+
+?>
