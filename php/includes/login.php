61f4573dbd4035fc7715ba2fc89e60dbc145add3
Abhisehk_Gupta <knoxxs@hotmail.com>
2013-03-24 16:33:32 +0530

    done login

--- php/includes/login.php
+++ php/includes/login.php
@@ -1,61 +1,96 @@
 <?
+require_once __DIR__.'/base.php';
+require_once __DIR__.'/user.php';
 
-require_once './initialize_database.php';
-
-//TODO: Make password MD5
-class Login
+class Login extends Base
 { 
-	private $userName, $password;
+	private $userName, $password, $user;
 
-	function __construct($userName,$password)
+	function __construct($userName,$password='')
 	{
+		session_start();
+		parent::__construct();
 		$this->userName = $userName;
-		$this->password = ($password);
+		$this->password = md5($password);
+		$user = null;
 	}
-
-	public function __destruct(){
-
-	}
-
-	public function __toString(){
-		return print_r($this);
-	}
-
-	private function getDb(){
-		if(!isset($this->db)){
-			$this->db = (new Database())->connectToDatabase();
-			return $this->db;
-		}else{
-			return $this->db;
-		}
-	}
-
 	
-	public function validateAndLogin(){ 
-		$db = $this->getDb();
-		$db->query('SELECT password From User WHERE userName=?',array($this->userName));
-		$records = $db->fetch_assoc_all();
-		$nrows = $db->returned_rows;
-		
-		if($nrows==0){
-			return 404;		
-		}
-		else if($nrows==1){
-			if($this->password == $result[0]['password']){
-				return 200;
-			}
-			else{
-				return 202;
-			}
-		}
+	public function validateData(){
+		return ($this->validateVar($this->userName) && $this->validateVar($this->password));
 	}
 
-	public static function isLoggedIn(){
-		if($_SESSION[])
+	public function login(){
+		if($this->validateData()){
+			if(!$this->isLoggedIn()){
+				$db = $this->getDb();
+				$db->query('SELECT password From User WHERE userName=?',array($this->userName));
+				$records = $db->fetch_assoc_all();
+				$nrows = $db->returned_rows;
+
+				if($nrows==0){
+					$this->head['status'] = 404;
+					$this->head['message'] = 'Wrong Username';
+				}
+				else if($nrows==1){
+					if($this->password == $records[0]['password']){
+						$this->head['status'] = 200;
+
+						$this->user = new User($this->userName);
+						$_SESSION['user'] = $this->user;
+					}
+					else{
+						$this->head['status'] = 404;
+						$this->head['message'] = 'Wrong Password';
+					}
+				}else{
+					//TODO:LOG this is a server/data fault
+				}
+			}else{
+				if($_SESSION['user']->getUserName() == $this->userName){
+					$this->head['status'] = 200;
+					$this->head['message'] = 'Already Logged in';
+				}else{
+					$this->logout();
+					$this->head['status'] = 409;
+					$this->head['message'] = 'Already logged with another account, logging out';
+				}
+			}
+		}else{
+			$this->head['status'] = 203;
+		}
+		return true;
 	}
+
+	public function logout(){
+		if($this->validateVar($this->userName)){
+			if($this->isLoggedIn()){
+				if($_SESSION['user']->getUserName() == $this->userName){
+					session_destroy();
+					$this->head['status'] = 200;
+				}else{
+					session_destroy();
+					$this->head['status'] = 409;
+					$this->head['message'] = 'Logged in with another account, logging out';
+				}
+			}else {
+				$this->head['status'] = 405;
+			}
+		}else{
+			$this->head['status'] = 203;			
+		}
+	
+		return true;
+	}
+
+	public function toJson(){
+		if($this->validateVar($_SESSION)){
+			$this->body['firstName'] = $_SESSION['user']->getFirstName();
+			$this->body['lastName'] = $_SESSION['user']->getLastName();
+			$this->body['reputation'] = $_SESSION['user']->getReputation();
+			$this->body['isReviewer'] = $_SESSION['user']->getIsReviewer();
+		}
+		return json_encode($this->result);
+	}
+
 }
-
-
-$login = new Login("uname2","123");
-echo $login->validateAndLogin();
 ?>
